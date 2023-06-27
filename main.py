9399cbf236aef2cb354e9003dfaefda1639496cd
Hubert Musiał (hmusial) <hmusial@cisco.com>
2023-06-27 16:39:17 +0200

    beta version

--- main.py
+++ main.py
@@ -1,100 +1,182 @@
-from deap import base, algorithms
-from deap import creator
-from deap import tools
-
-from graph_show import show_graph
-
 import random
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+from deap import base, creator, tools, algorithms
 
-inf = 100
-D = ((0, 3, 1, 3, inf, inf),
-     (3, 0, 4, inf, inf, inf),
-     (1, 4, 0, inf, 7, 5),
-     (3, inf, inf, 0, inf, 2),
-     (inf, inf, 7, inf, 0, 4),
-     (inf, inf, 5, 2, 4, 0))
+# Problem Constants
+NUM_CITIES = 10
 
-start = 0
-end = 5
-LENGTH_D = len(D)
-LENGTH_CHROM = len(D)*len(D[0])
+# Genetic Algorithm Constants
 POPULATION_SIZE = 1000
-P_CROSSOVER = 0.9
-P_MUTATION = 0.1
-MAX_GENERATIONS = 30
-HALL_OF_FAME_SIZE = 1
+NUM_GENERATIONS = 10
+CROSSOVER_PROB = 0.5
+MUTATION_PROB = 0.1
 
-hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
+city_coordinates = [
+    (1, 2), (2, 6), (6, 6), (6, 2), (3, 14),
+    (10, 10), (15, 5), (15, 17), (17, 15), (19, 19)
+]
 
-RANDOM_SEED = 42
-random.seed(RANDOM_SEED)
+connections = {
+    0: [1, 2],
+    1: [0, 2, 4],
+    2: [0, 1, 3],
+    3: [2],
+    4: [1, 5, 7],
+    5: [2, 4, 6, 7],
+    6: [5, 9],
+    7: [4, 5, 8, 9],
+    8: [7],
+    9: [6, 7]
+}
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+# Create the graph
+G = nx.Graph()
+
+# Add nodes (cities) to the graph
+G.add_nodes_from(range(NUM_CITIES))
+
+for city, connected_cities in connections.items():
+    for connected_city in connected_cities:
+        G.add_edge(city, connected_city)
+
+# Create the Fitness and Individual classes
+creator.create("FitnessMin", base.Fitness, weights=(-0.1,))
 creator.create("Individual", list, fitness=creator.FitnessMin)
 
+# Initialize the toolbox
 toolbox = base.Toolbox()
-toolbox.register("randomOrder", random.sample, range(LENGTH_D), LENGTH_D)
-toolbox.register("individualCreator", tools.initRepeat, creator.Individual, toolbox.randomOrder, LENGTH_D)
-toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
 
-population = toolbox.populationCreator(n=POPULATION_SIZE)
+# Attribute generator
+toolbox.register("attribute", random.sample, range(NUM_CITIES), NUM_CITIES)
 
+# Structure initializers
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attribute)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-def dikstryFitness(individual):
-    s = 0
-    for n, path in enumerate(individual):
-        path = path[:path.index(n)+1]
+def evaluate_fitness(individual):
+    # Calculate the total distance between cities
+    distance = 0
+    for i in range(NUM_CITIES - 1):
+        city1_index = individual[i]
+        city2_index = individual[i + 1]
+        x1, y1 = city_coordinates[city1_index]
+        x2, y2 = city_coordinates[city2_index]
+        distance += np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        
+        # Check if the connection exists between the current cities
+        if city2_index not in connections[city1_index]:
+            # Add a penalty for not following the predefined connection
+            distance += 999999999999999
 
-        si = start
-        for j in path:
-            s += D[si][j]
-            si = j
-
-    return s,
-
-def cxOrdered(ind1, ind2):
-    for p1, p2 in zip(ind1, ind2):
-        tools.cxOrdered(p1, p2)
-
-    return ind1, ind2
-
-def mutShuffleIndexes(individual, indpb):
-    for ind in individual:
-        tools.mutShuffleIndexes(ind, indpb)
-
-    return individual,
+    return distance,
 
 
-toolbox.register("evaluate", dikstryFitness)
+# Register the evaluation function
+toolbox.register("evaluate", evaluate_fitness)
+
+# Genetic operators
+toolbox.register("mate", tools.cxPartialyMatched)
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("mate", cxOrdered)
-toolbox.register("mutate", mutShuffleIndexes, indpb=1.0/LENGTH_CHROM/10)
 
+# Create the statistics and Hall of Fame objects
 stats = tools.Statistics(lambda ind: ind.fitness.values)
 stats.register("min", np.min)
 stats.register("avg", np.mean)
 stats.register("max", np.max)
 
-population, logbook = algorithms.eaSimple(population, toolbox,
-                                        cxpb=P_CROSSOVER/LENGTH_D,
-                                        mutpb=P_MUTATION/LENGTH_D,
-                                        ngen=MAX_GENERATIONS,
-                                        halloffame=hof,
-                                        stats=stats,
-                                        verbose=True)
+hof = tools.HallOfFame(1)
 
-minFitnessValues, meanFitnessValues, maxFitnessValues = logbook.select("min", "avg", "max")
+# Create the initial population
+population = toolbox.population(n=POPULATION_SIZE)
 
-plt.plot(maxFitnessValues, color='red', label='Maximum')
-plt.plot(meanFitnessValues, color='green', label='Average')
-plt.plot(minFitnessValues, color='blue', label='Minimum')
-plt.xlabel('Generation')
-plt.ylabel('Max/Average/Min')
-plt.title('Values of each generations')
-plt.legend(loc="lower right")
-fig, ax = plt.subplots()
-best = hof.items[0]
-show_graph(ax, best, start, end)
+# Create the logbook to store the evolution statistics
+logbook = tools.Logbook()
+logbook.header = ["gen", "nevals"] + stats.fields
+
+# Perform the evolution
+for generation in range(NUM_GENERATIONS):
+    # Evaluate fitness for all individuals in the population
+    fitness_values = map(toolbox.evaluate, population)
+    for individual, fitness_value in zip(population, fitness_values):
+        individual.fitness.values = fitness_value
+    
+    # Perform genetic operations
+    offspring = toolbox.select(population, len(population))
+    offspring = algorithms.varAnd(offspring, toolbox, cxpb=CROSSOVER_PROB, mutpb=MUTATION_PROB)
+    
+    # Update fitness values for the offspring
+    fitness_values = map(toolbox.evaluate, offspring)
+    for child, fitness_value in zip(offspring, fitness_values):
+        child.fitness.values = fitness_value
+    
+    # Replace the population with the offspring
+    population[:] = offspring
+    
+    # Update the Hall of Fame
+    hof.update(population)
+    
+    # Gather statistics
+    record = stats.compile(population)
+    logbook.record(gen=generation, nevals=len(population), **record)
+
+# Print the best individual and its fitness value
+best_individual = hof[0]
+best_fitness = evaluate_fitness(best_individual)[0]
+print("Best Individual:", best_individual)
+print("Best Fitness:", best_fitness)
+
+# Plot the evolution statistics and the best individual's route
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 6))
+
+# Evolution statistics
+min_fitness_values = logbook.select("min")
+avg_fitness_values = logbook.select("avg")
+max_fitness_values = logbook.select("max")
+
+ax1.plot(min_fitness_values, color="blue", label="Min")
+ax1.plot(avg_fitness_values, color="green", label="Avg")
+ax1.plot(max_fitness_values, color="red", label="Max")
+ax1.set_xlabel("Generation")
+ax1.set_ylabel("Fitness")
+ax1.legend(loc="best")
+ax1.set_title("Evolution of Fitness")
+ax1.grid(True)
+
+# Best individual's route
+G_best = nx.Graph()
+G_best.add_nodes_from(range(NUM_CITIES))
+for i in range(NUM_CITIES):
+    for j in range(i + 1, NUM_CITIES):
+        G_best.add_edge(i, j)
+
+# Draw all nodes with their numbers
+pos = {i: city_coordinates[i] for i in range(NUM_CITIES)}
+nx.draw_networkx_nodes(G_best, pos=pos, ax=ax2)
+nx.draw_networkx_labels(G_best, pos=pos, font_color="black", ax=ax2)
+
+# Draw the connections between cities
+edges = G.edges
+nx.draw_networkx_edges(G, pos=pos, edgelist=edges, edge_color="black", ax=ax2)
+
+# Draw all nodes with their numbers
+pos = {i: city_coordinates[i] for i in range(NUM_CITIES)}
+nx.draw_networkx_nodes(G_best, pos=pos, ax=ax3)
+nx.draw_networkx_labels(G_best, pos=pos, font_color="black", ax=ax3)
+edges = G.edges
+nx.draw_networkx_edges(G, pos=pos, edgelist=edges, edge_color="black", ax=ax3)
+
+# Draw the shortest path in red
+
+shortest_path = best_individual
+shortest_path_edges = [(shortest_path[i], shortest_path[i + 1]) for i in range(len(shortest_path) - 1)]
+print(f'shpe: {shortest_path_edges}')
+nx.draw_networkx_edges(G_best, pos=pos, edgelist=shortest_path_edges, edge_color="red", ax=ax3)
+
+ax2.set_title("Connection sheme")
+ax3.set_title("Best Individual's Route")
+
+plt.tight_layout()
 plt.show()
